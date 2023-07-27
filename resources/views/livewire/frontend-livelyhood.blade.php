57a606c936fe879f040adbbba9f933e3c8df48cb
ultramenid <malichamdan@gmail.com>
2023-07-28 00:49:18 +0700

    new layout

--- resources/views/livewire/frontend-livelyhood.blade.php
+++ resources/views/livewire/frontend-livelyhood.blade.php
@@ -4,9 +4,9 @@
             @foreach ($livelyhood as $item)
 
                 <div class="bg-gray-100 pb-6 sm:w-3/12">
-                    <img class="w-full h-60 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
+                    <img class="w-full h-44 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
                     <div class="px-4">
-                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" md:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
+                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" sm:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
                         <div class="mt-6">
                             <h1 class="sm:text-base text-sm">
                             <span class="font-bold ">
@@ -24,77 +24,6 @@
                     </div>
 
                 </div>
-                <div class="bg-gray-100 pb-6 sm:w-3/12">
-                    <img class="w-full h-60 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
-                    <div class="px-4">
-                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" md:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
-                        <div class="mt-6">
-                            <h1 class="sm:text-base text-sm">
-                            <span class="font-bold ">
-                                @php
-                                $date = \Carbon\Carbon::parse($item->publishdate)->locale('id');
-                                $date->settings(['formatFunction' => 'translatedFormat']);
-                                echo $date->format('d F Y');
-                                @endphp
-                            </span>
-                            <span> | </span>
-                            {{$item->deskripsi}}
-                            </h1>
-
-                        </div>
-                    </div>
-
-                </div>
-                <div class="bg-gray-100 pb-6 sm:w-3/12">
-                    <img class="w-full h-60 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
-                    <div class="px-4">
-                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" md:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
-                        <div class="mt-6">
-                            <h1 class="sm:text-base text-sm">
-                            <span class="font-bold ">
-                                @php
-                                $date = \Carbon\Carbon::parse($item->publishdate)->locale('id');
-                                $date->settings(['formatFunction' => 'translatedFormat']);
-                                echo $date->format('d F Y');
-                                @endphp
-                            </span>
-                            <span> | </span>
-                            {{$item->deskripsi}}
-                            </h1>
-
-                        </div>
-                    </div>
-
-                </div>
-                <div class="bg-gray-100 pb-6 sm:w-3/12">
-                    <img class="w-full h-60 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
-                    <div class="px-4">
-                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" md:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
-                        <div class="mt-6">
-                            <h1 class="sm:text-base text-sm">
-                            <span class="font-bold ">
-                                @php
-                                $date = \Carbon\Carbon::parse($item->publishdate)->locale('id');
-                                $date->settings(['formatFunction' => 'translatedFormat']);
-                                echo $date->format('d F Y');
-                                @endphp
-                            </span>
-                            <span> | </span>
-                            {{$item->deskripsi}}
-                            </h1>
-
-                        </div>
-                    </div>
-
-                </div>
-
-
-
-
-
-
-
-
 
             @endforeach
 
