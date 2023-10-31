9a129bb282c8a14faed7634de995986bde488d86
ultramenid <malichamdan@gmail.com>
2023-10-31 10:28:49 +0700

    updated posts flow

--- resources/views/livewire/frontend-livelyhood.blade.php
+++ resources/views/livewire/frontend-livelyhood.blade.php
@@ -1,9 +1,9 @@
 <div class=" max-w-6xl mx-auto py-6 ">
-    <div class=" flex flex-wrap sm:flex-row flex-col sm:space-x-10 space-x-0 sm:space-y-0 space-y-10 justify-center   sm:px-0 px-4">
+    <div class=" flex flex-wrap sm:flex-row flex-col sm:space-x-10 space-x-0 sm:space-y-6 space-y-10 justify-center   sm:px-0 px-4">
         @if ($livelyhood)
             @foreach ($livelyhood as $item)
 
-                <div class="bg-gray-100 pb-6 sm:w-3/12">
+                <div class="bg-gray-100 pb-6 sm:w-3/12 ">
                     <img class="w-full h-44 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
                     <div class="px-4">
                         <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" sm:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
@@ -28,6 +28,7 @@
             @endforeach
 
         @endif
+
     </div>
     @if ($livelyhood)
         {{ $livelyhood->links('livewire.pagination') }}
--- resources/views/livewire/frontend-risetdan-kajian.blade.php
+++ resources/views/livewire/frontend-risetdan-kajian.blade.php
@@ -1,18 +1,20 @@
 <div class=" max-w-6xl mx-auto py-6 ">
-    <div class=" grid grid-flow-row md:grid-cols-3 grid-cols-1 gap-4  sm:px-0 px-4">
+    <div class=" flex flex-wrap sm:flex-row flex-col sm:space-x-10 space-x-0 sm:space-y-6 space-y-10 justify-center   sm:px-0 px-4">
         @if ($risetdankajian)
             @foreach ($risetdankajian as $item)
 
-                <div class="bg-gray-100 pb-6 w-full">
-                    <img class="w-full h-60 object-cover object-center" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
-                    <a href="{{ url('risetdankajian', [$item->id, $item->slug]) }}" class="mt-6 md:text-2xl text-xl font-bold  text-auriga-biru px-4">{{$item->title}}</a>
-                        <div class="px-4 mt-4">
+
+                <div class="bg-gray-100 pb-6 sm:w-3/12 ">
+                    <img class="w-full h-44 object-cover object-center mb-6" src="{{ asset('storage/files/photos/'.$item->img) }}" alt="{{$item->title}}" />
+                    <div class="px-4">
+                        <a href="{{ url('livelihood', [$item->id, $item->slug]) }}" class=" sm:text-2xl text-xl font-bold mt-6">{{$item->title}}</a>
+                        <div class="mt-6">
                             <h1 class="sm:text-base text-sm">
                             <span class="font-bold ">
                                 @php
                                 $date = \Carbon\Carbon::parse($item->publishdate)->locale('id');
                                 $date->settings(['formatFunction' => 'translatedFormat']);
-                                echo $date->format('F Y');
+                                echo $date->format('d F Y');
                                 @endphp
                             </span>
                             <span> | </span>
@@ -20,6 +22,8 @@
                             </h1>
 
                         </div>
+                    </div>
+
                 </div>
 
             @endforeach
